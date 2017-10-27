e9003e94f7b74cc26c27680ab8b91265f8511614
Katherine Pachal <kpachal@11-s85-v14-wlan.triumf.ca>
2017-10-27 14:18:24 -0700

    Initial Commit

---
+++ katetest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  katetest
+//
+//  Created by Katherine Pachal on 27.10.17.
+//  Copyright © 2017 Katherine Pachal. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
